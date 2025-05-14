818967d101d0bbe643e466dfe45470d6e2e4b2c8
sohan <sohan@sohannikam>
2025-05-14 13:11:24 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		String hubgry ="yes";
+		
+		if(hubgry=="yes")
+		{
+			System.out.println("set something");
+		}
+		else {
+			System.out.println("dont set anyting");
+		}
+	}
+}
