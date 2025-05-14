0ade538cefe7752f3a956cd1b153970ad2be67fc
sohan <sohan@sohannikam>
2025-05-14 13:27:52 +0530

    First D commit

---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		String hubgry ="yes";
+		
+		if(hubgry=="No")
+		{
+			System.out.println("set something");
+		}
+		else {
+			System.out.println("dont set anyting");
+		}
+	}
+}
